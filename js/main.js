4d8e37f14d14528622aa9ec2f0ec4087f7fc9e51
jiayuanyuan <jiayuanyuan11260516@163.com>
2016-11-15 16:14:47 +0800

    updata

--- js/main.js
+++ js/main.js
@@ -14,11 +14,21 @@ require.config({
 	}
 })
 require(['jquery', 'pImg', 'swiper', 'page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'formSubmit'], function($, pImg, Swiper, page1, page2, page3, page4, page5, page6, formSubmit) {
-	var handle = [page1, page2, page3, page4, page5, page6];
-	handle.data = {};
+	$('body').css('height',$(window).height());
+	$('body').css('width',$(window).width());
+	var pageHandle = [page1, page2, page3, page4, page5, page6];
+	pageHandle.data = {};
 	var mySwiper = new Swiper('#pages', {
 		direction: 'vertical',
-		speed: 700
+		speed: 700,
+		onSlideChangeEnd: function(_swiper){
+             var index = _swiper.activeIndex,
+                module = pageHandle[index];
+            module && module['init'](_swiper, pageHandle);
+       },
+       onInit:function(_swiper){
+       	pageHandle[0]['init'](_swiper,pageHandle);
+       }
 
 	})
 
--- js/page1.js
+++ js/page1.js
@@ -1,4 +1,33 @@
-define(['jquery'],function($){
+define(['jquery'], function($){
 	
 	
+	var main = {
+		hasInit:false,
+		init: function(swiper,routerObj){
+			var that=this;
+			that.swiper=swiper;
+			that.routerObj=routerObj;
+			if(!that.hasInit){
+				that._init();
+				that.hasInit= true;
+			}
+		},
+		_init: function(){
+			var that=this;
+			var page1Swiper = new Swiper('#page1', {
+                    effect : 'coverflow',
+                    slidesPerView: 3,
+                    centeredSlides: true,
+                    coverflow: {
+                        rotate: 0,
+                        stretch: -24,
+                        depth: 120,
+                        modifier: 1,
+                        slideShadows: true
+                    }
+			})
+		}
+	}
+	return main;
+
 })
